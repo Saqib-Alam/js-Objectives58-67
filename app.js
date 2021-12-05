78ad91840554bf76eb0e3b87adb49a4d4970ac6c
Saqib Alam <saqibalam64@gmail.com>
2021-12-06 00:26:56 +0500

    Objectives

---
+++ app.js
@@ -0,0 +1,80 @@
+// Chapter 58-67
+
+
+// ques1
+
+// (i)
+// var mainContent=document.getElementById('main-content');
+
+// (ii)
+// console.log(mainContent.childNodes)
+
+// (iii)
+// var render=document.getElementsByClassName("render")
+// console.log(render)
+
+// (iv)
+// var firstName=document.getElementById("first-name");
+// firstName.value='Saqib';
+
+// (v)
+// var lastName=document.getElementById("last-name");
+// lastName.value='Alam'
+
+// var email=document.getElementById("email");
+// email.value='saqibalam64@gmail.com'
+
+
+
+
+
+// var target = document.getElementById("div1");
+// var attVal = target.getAttribute("class");
+
+
+
+// ques2
+// (i)
+// var form=document.getElementById("form-content")
+// console.log(form.nodeType)
+
+//(ii) 
+// var lastName=document.getElementById("lastName")
+// console.log(lastName.nodeType)
+
+
+// (iii)
+// var lastName=document.getElementById("lastName")
+// console.log(lastName.innerHtml="Hassan")
+
+
+
+// (iv)
+
+// mainContent=document.getElementById("main-content")
+// var firstChild=mainContent.firstChild;
+// var lastChild=mainContent.lastChild;
+// console.log(firstChild)
+// console.log(lastChild)
+
+
+// (v)
+
+// var f=document.getElementById("lastName")
+
+// var preSibling=f.previousSibling;
+// var nextSibling=f.nextSibling;
+// console.log(preSibling)
+// console.log(nextSibling)
+
+
+// (vi)
+
+// var email=document.getElementById("email")
+
+// var pNode=email.parentNode;
+// var tNode=email.nodeType;
+
+// console.log(pNode)
+// console.log(tNode)
+
